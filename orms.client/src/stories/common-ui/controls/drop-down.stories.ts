78c1883c4c1d3ae7084ac8e5bbb58a03192d104d
Jangili Rao <j.rao@edynamix.com>
2025-10-05 23:05:18 +0530

    Dropdown story created

---
+++ orms.client/src/stories/common-ui/controls/drop-down.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { fn } from 'storybook/test';
+import { DropDownComponent } from '../../../app/common-ui/controls/drop-down/drop-down.component';
+
+const meta: Meta<DropDownComponent> = {
+  title: 'Common-UI/Dropdown',
+  component: DropDownComponent,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  args: {},
+};
+
+export default meta;
+type Story = StoryObj<DropDownComponent>;
+
+export const Dropdown: Story = {};
