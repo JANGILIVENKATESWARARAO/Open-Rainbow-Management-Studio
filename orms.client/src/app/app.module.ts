83a7a21fa59dc5f04bb2afcc7203cadd3877aff5
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-07 12:26:23 +0530

    Add FileUpload component with initial design and styling

--- orms.client/src/app/app.module.ts
+++ orms.client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DropDownComponent } from './common-ui/controls/drop-down/drop-down.component';
+import { FileUploadComponent } from './common-ui/controls/file-upload/file-upload.component';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
         BrowserModule, HttpClientModule,
         AppRoutingModule,
-        DropDownComponent
+        DropDownComponent,
+        FileUploadComponent
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ orms.client/src/stories/common-ui/controls/file-upload.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { fn } from 'storybook/test';
+import { FileUploadComponent } from '../../../app/common-ui/controls/file-upload/file-upload.component';
+
+
+const meta: Meta<FileUploadComponent> = {
+  title: 'Common-UI/FileUpload',
+  component: FileUploadComponent,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  args: {},
+};
+
+export default meta;
+type Story = StoryObj<FileUploadComponent>;
+
+export const FileUpload: Story = {};
