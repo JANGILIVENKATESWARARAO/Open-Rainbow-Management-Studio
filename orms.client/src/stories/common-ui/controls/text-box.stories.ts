f39eb7d9bf1643aa3bd553f21ff1dd27fc0bee94
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-07 15:26:01 +0530

    Add Text Box component with initial design and styling

---
+++ orms.client/src/stories/common-ui/controls/text-box.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { fn } from 'storybook/test';
+import { TextBoxComponent } from '../../../app/common-ui/controls/text-box/text-box.component';
+
+
+const meta: Meta<TextBoxComponent> = {
+  title: 'Common-UI/Textbox',
+  component: TextBoxComponent,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  args: {},
+};
+
+export default meta;
+type Story = StoryObj<TextBoxComponent>;
+
+export const Textbox: Story = {};
