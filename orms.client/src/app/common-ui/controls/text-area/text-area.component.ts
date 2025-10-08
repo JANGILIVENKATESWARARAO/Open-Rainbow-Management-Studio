a5ecde48eb1648370fed96111f1ebad6d410e1f4
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-08 15:58:03 +0530

    Made Text Area component dynamic and UI Enhancement

--- orms.client/src/app/common-ui/controls/text-area/text-area.component.ts
+++ orms.client/src/app/common-ui/controls/text-area/text-area.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-text-area',
   standalone: true,
   templateUrl: './text-area.component.html',
-  styleUrl: './text-area.component.css'
+  styleUrl: './text-area.component.css',
+  imports: [CommonModule]
 })
 export class TextAreaComponent {
-
+  @Input() label: string = 'Address';
+  @Input() Placeholder: string = 'Enter your address';
+  @Input() isRequired: boolean = true;
 }
