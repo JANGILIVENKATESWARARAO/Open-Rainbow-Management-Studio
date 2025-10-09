4f8af48ed671eab1fccbb8fb839f0dd359229311
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-09 10:43:37 +0530

    changed the text-box in dynamic

--- orms.client/src/app/common-ui/controls/text-box/text-box.component.ts
+++ orms.client/src/app/common-ui/controls/text-box/text-box.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component,Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-text-box',
   standalone: true,
   templateUrl: './text-box.component.html',
-  styleUrl: './text-box.component.css'
+  styleUrl: './text-box.component.css',
+  imports: [CommonModule]
 })
 export class TextBoxComponent {
+  @Input() label: string = 'fullName';
+    @Input() placeHolder: string = 'Enter full name';
+    @Input() isRequired: boolean = false;
 
 }
