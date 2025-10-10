7a6665fc9534e2f6823a3e7295fbe6e88db215ca
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-10 09:19:14 +0530

    Updated Dropdown Design

--- orms.client/src/app/common-ui/controls/drop-down/drop-down.component.ts
+++ orms.client/src/app/common-ui/controls/drop-down/drop-down.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 
+export interface DropDown {
+  name: string;
+  value: any;
+}
 @Component({
-    selector: 'app-drop-down',
-    templateUrl: './drop-down.component.html',
-    styleUrl: './drop-down.component.css',
-    standalone: true
+  selector: 'app-drop-down',
+  templateUrl: './drop-down.component.html',
+  styleUrl: './drop-down.component.css',
+  standalone: true,
+  imports: [CommonModule],
 })
 export class DropDownComponent {
+  @Input() label: string = 'Gender';
+  @Input() placeHolder: string = 'Select Gender';
+  @Input() isRequired: boolean = false;
+  @Output() selectedChange = new EventEmitter<any>();
+  @Input() options: DropDown[] = [];
 
+  selectedValue: any;
+  selectOption(option: DropDown) {
+    this.selectedValue = option.name;
+    this.selectedChange.emit(option.value);
+  }
 }
