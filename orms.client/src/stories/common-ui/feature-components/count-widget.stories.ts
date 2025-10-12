993daaa08d86a1c70f5512e2357a425fb57229ce
Jangili Rao <j.rao@edynamix.com>
2025-10-12 09:18:19 +0530

    Autodocs commented and folder structure changed

--- orms.client/src/stories/common-ui/feature-components/count-widget.stories.ts
+++ orms.client/src/stories/common-ui/feature-components/count-widget.stories.ts
@@ -1,10 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/angular';
-import { CountWidgetComponent } from '../../app/common-ui/feature-components/count-widget/count-widget.component';
-
-
+import { CountWidgetComponent } from '../../../app/common-ui/feature-components/count-widget/count-widget.component';
 
 const meta: Meta<CountWidgetComponent> = {
-  title: 'Feature Components/Count-widget',
+  title: 'Feature Components/CountWidget',
   component: CountWidgetComponent,
   // tags: ['autodocs'],
   parameters: {
@@ -16,4 +14,4 @@ const meta: Meta<CountWidgetComponent> = {
 export default meta;
 type Story = StoryObj<CountWidgetComponent>;
 
-export const countWidget: Story = {};
+export const CountWidget: Story = {};
