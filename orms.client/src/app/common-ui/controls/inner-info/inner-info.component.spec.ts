0e61fdb1cdba8e007efbcd367b4ce6ca5b49730f
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-15 12:23:54 +0530

    Added Inner-info component and storybook

---
+++ orms.client/src/app/common-ui/controls/inner-info/inner-info.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InnerInfoComponent } from './inner-info.component';
+
+describe('InnerInfoComponent', () => {
+  let component: InnerInfoComponent;
+  let fixture: ComponentFixture<InnerInfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [InnerInfoComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InnerInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
