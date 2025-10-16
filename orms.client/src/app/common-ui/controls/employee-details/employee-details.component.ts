1e3cf4875aa739a154c0c170dd268b0276fd798b
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-16 22:08:20 +0530

    In Employee-details added basic-info,contact-info,job-details,Education-details

--- orms.client/src/app/common-ui/controls/employee-details/employee-details.component.ts
+++ orms.client/src/app/common-ui/controls/employee-details/employee-details.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
-
+import { InnerInfoComponent } from '../inner-info/inner-info.component';
+import { InnerContactComponent } from '../inner-contact/inner-contact.component';
+import { InnerEducationComponent } from '../inner-education/inner-education.component';
+import { InnerJobComponent } from '../inner-job/inner-job.component';
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.css'],
   standalone: true,
-  imports: [ButtonComponent]
+  imports: [ButtonComponent,InnerInfoComponent,InnerContactComponent,InnerJobComponent,InnerEducationComponent]
 })
 export class EmployeeDetailsComponent {
   printDetails() {
