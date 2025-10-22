7fb9956026fa0163cd360c928d13493b7ec20617
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-22 18:03:52 +0530

    changing the location of leave-update storybook

--- orms.client/src/stories/modules/dashboard/leave-update.stories.ts
+++ orms.client/src/stories/modules/dashboard/leave-update.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/angular';
 import { LeaveUpdateComponent } from '../../../app/modules/dashboard/leave-update/leave-update.component';
 
 const meta: Meta<LeaveUpdateComponent> = {
-  title: 'Feature Components/LeaveUpdate',
+  title: 'Module/Dashboard/LeaveUpdate',
   component: LeaveUpdateComponent,
   // tags: ['autodocs'],
   parameters: {
