d4029124ebc7b0ca0ccb2cc3167f5adf4e88d544
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-22 17:45:44 +0530

    Replacing Text area with text box in document-upload component

--- orms.client/src/app/modules/add-employee/documents-upload/documents-upload.component.ts
+++ orms.client/src/app/modules/add-employee/documents-upload/documents-upload.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { FileUploadComponent } from '../../../common-ui/controls/file-upload/file-upload.component';
 import { TextAreaComponent } from '../../../common-ui/controls/text-area/text-area.component';
+import { TextBoxComponent } from "../../../common-ui/controls/text-box/text-box.component";
 
 @Component({
   selector: 'app-documents-upload',
   templateUrl: './documents-upload.component.html',
   styleUrl: './documents-upload.component.css',
   standalone: true,
-  imports:[FileUploadComponent, TextAreaComponent]
+  imports: [FileUploadComponent, TextAreaComponent, TextBoxComponent]
 })
 export class DocumentsUploadComponent {
 
