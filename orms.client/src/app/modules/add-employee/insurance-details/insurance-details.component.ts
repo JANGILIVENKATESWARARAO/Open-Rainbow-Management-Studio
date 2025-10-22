1ca0ed8020acab7a35f3b28b15b29ffa2111b525
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-22 17:46:31 +0530

    Replacing text area to text box cin insurance-details component

--- orms.client/src/app/modules/add-employee/insurance-details/insurance-details.component.ts
+++ orms.client/src/app/modules/add-employee/insurance-details/insurance-details.component.ts
@@ -3,13 +3,14 @@ import { FileUploadComponent } from '../../../common-ui/controls/file-upload/fil
 import { TextAreaComponent } from '../../../common-ui/controls/text-area/text-area.component';
 import { CalendarComponent } from '../../../common-ui/controls/calendar/calendar.component';
 import { ToggleComponent } from '../../../common-ui/controls/toggle/toggle.component';
+import { TextBoxComponent } from "../../../common-ui/controls/text-box/text-box.component";
 
 @Component({
   selector: 'app-insurance-details',
   templateUrl: './insurance-details.component.html',
   styleUrl: './insurance-details.component.css',
   standalone: true,
-  imports:[FileUploadComponent, TextAreaComponent,CalendarComponent,ToggleComponent]
+  imports: [FileUploadComponent, TextAreaComponent, CalendarComponent, ToggleComponent, TextBoxComponent]
 })
 export class InsuranceDetailsComponent {
 
