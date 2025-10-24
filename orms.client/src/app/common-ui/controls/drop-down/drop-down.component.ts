77046e709e1a563e1b0c69fb72354809a5ad240e
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-24 17:55:18 +0530

    Implemented to dropdown up and down functionality of drop-down component

--- orms.client/src/app/common-ui/controls/drop-down/drop-down.component.ts
+++ orms.client/src/app/common-ui/controls/drop-down/drop-down.component.ts
@@ -6,7 +6,6 @@ import {
   HostListener,
   Input,
   Output,
-  SimpleChanges,
 } from '@angular/core';
 
 export interface DropDown {
@@ -28,13 +27,14 @@ export class DropDownComponent {
   @Input() options: DropDown[] = [];
 
   selectedValue: string | null = null;
+  isOpenUp: boolean = false;
 
   constructor(private eRef: ElementRef) {}
   selectOption(option: DropDown) {
     this.selectedValue = option.name;
     this.selectedChange.emit(option.value);
-
-    setTimeout(() => {
+    
+        setTimeout(() => {
       this.closeDropdown();
     }, 0);
   }
@@ -43,6 +43,43 @@ export class DropDownComponent {
   handleClickOutside(event: MouseEvent) {
     if (!this.eRef.nativeElement.contains(event.target)) {
       this.closeDropdown();
+    }
+  }
+
+  @HostListener('change', ['$event'])
+  onToggle(event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    if (checkbox.checked) {
+      this.decideDropdownDirection();
+    }
+  }
+
+  private decideDropdownDirection() {
+    const dropdown = this.eRef.nativeElement.querySelector(
+      '.custom-dropdown'
+    ) as HTMLElement;
+    const list = this.eRef.nativeElement.querySelector(
+      '.custom-dropdown-list'
+    ) as HTMLElement;
+
+    if (!dropdown || !list) return;
+
+    list.style.visibility = 'hidden';
+    list.style.display = 'block';
+    const listHeight = list.offsetHeight || 150;
+    list.style.display = '';
+    list.style.visibility = '';
+
+    const rect = dropdown.getBoundingClientRect();
+    const spaceBelow = window.innerHeight - rect.bottom;
+    const spaceAbove = rect.top;
+
+    this.isOpenUp = spaceBelow < listHeight && spaceAbove > spaceBelow;
+
+    if (this.isOpenUp) {
+      list.classList.add('open-up');
+    } else {
+      list.classList.remove('open-up');
     }
   }
 
