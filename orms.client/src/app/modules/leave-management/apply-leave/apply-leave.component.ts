de4cdca21344b63dfd9a6f64c54cfcbb079e811f
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-28 18:13:54 +0530

    Add Apply leave button and leave type dropdown in apply-leave component

--- orms.client/src/app/modules/leave-management/apply-leave/apply-leave.component.ts
+++ orms.client/src/app/modules/leave-management/apply-leave/apply-leave.component.ts
@@ -3,13 +3,29 @@ import { NoticeBannerComponent } from '../../../common-ui/feature-components/not
 import { CalendarComponent } from '../../../common-ui/controls/calendar/calendar.component';
 import { TextAreaComponent } from '../../../common-ui/controls/text-area/text-area.component';
 import { ButtonComponent } from '../../../common-ui/controls/button/button.component';
+import { DropDownComponent } from '../../../common-ui/controls/drop-down/drop-down.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-apply-leave',
   templateUrl: './apply-leave.component.html',
   styleUrl: './apply-leave.component.css',
-  imports: [NoticeBannerComponent,CalendarComponent,TextAreaComponent,ButtonComponent],
+  imports: [NoticeBannerComponent,CalendarComponent,TextAreaComponent,ButtonComponent,DropDownComponent,CommonModule],
   standalone: true,
 })
 export class ApplyLeaveComponent {
+    showForm: boolean = false; 
+   
+
+  
+  showLeaveForm() {
+    
+    this.showForm = !this.showForm; 
+  }
+
+  submitApplication() {
+    this.showForm = false;
+  }
+  
+
 
 }
