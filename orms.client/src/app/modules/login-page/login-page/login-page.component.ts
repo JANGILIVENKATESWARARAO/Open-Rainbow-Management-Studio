e814f521e22581144a3cd05ab069edaad6decf68
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-28 12:44:02 +0530

    Replaced the edyn-checkbox with toggle and changed the width of text box

--- orms.client/src/app/modules/login-page/login-page/login-page.component.ts
+++ orms.client/src/app/modules/login-page/login-page/login-page.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../common-ui/controls/button/button.component';
 import { TextBoxComponent } from '../../../common-ui/controls/text-box/text-box.component';
-import { EdynCheckboxComponent } from '../../../common-ui/controls/edyn-checkbox/edyn-checkbox.component';
+import { ToggleComponent } from '../../../common-ui/controls/toggle/toggle.component';
 
 @Component({
   selector: 'app-login-page',
   standalone:true,
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
-  imports:[ButtonComponent,TextBoxComponent,EdynCheckboxComponent]
+  imports:[ButtonComponent,TextBoxComponent,ToggleComponent]
 })
 export class LoginPageComponent {
 
