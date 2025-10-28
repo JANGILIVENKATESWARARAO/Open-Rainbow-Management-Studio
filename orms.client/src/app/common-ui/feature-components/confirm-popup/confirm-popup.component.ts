7bdc9ef36fe634ea7cd12b034000619cd99a4c9a
Manu Maganti <manu.maganti@gmail.com>
2025-10-28 15:57:48 +0530

    Made the changes all the functionality with dynamic in confirm-popup component

--- orms.client/src/app/common-ui/feature-components/confirm-popup/confirm-popup.component.ts
+++ orms.client/src/app/common-ui/feature-components/confirm-popup/confirm-popup.component.ts
@@ -1,15 +1,19 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ButtonComponent } from '../../controls/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-confirm-popup',
   templateUrl: './confirm-popup.component.html',
   styleUrl: './confirm-popup.component.css',
   standalone: true,
+  imports: [ButtonComponent,CommonModule],
 })
 export class ConfirmPopupComponent {
-  @Input() label: string = 'Are you sure?';
+  @Input() label: string = 'You are about to permanently delete the employee: Sohan kumar. This action cannot be undone';
   @Input() yesButton: string = 'Yes';
   @Input() noButton: string = 'No';
-  @Input() title: string = '';
+  @Input() title: string = 'Are you sure?';
+  @Input() changeIcon: string = '';
+  @Input() borderColor: string = 'red';
 }
