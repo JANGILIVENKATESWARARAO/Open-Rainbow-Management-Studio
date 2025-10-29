aeec85fe54a55ce98205a7c38329ef75bd501283
JANGILIVENKATESWARARAO <jangilivenkateswarao@gmail.com>
2025-10-29 19:42:57 +0530

    pushed chip with dynamic size(sm,md,lg)

--- orms.client/src/app/common-ui/controls/chip/chip.component.ts
+++ orms.client/src/app/common-ui/controls/chip/chip.component.ts
@@ -1,18 +1,52 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-chip',
   standalone: true,
   templateUrl: './chip.component.html',
-  styleUrl: './chip.component.css',
+  styleUrls: ['./chip.component.css'],
 })
-export class ChipComponent {
+export class ChipComponent implements OnChanges {
   @Input() text: string = 'text';
   @Input() bgColor: string = 'white';
   @Input() color: string = 'gray';
   @Input() activeBGColor: string = '#7c3aedff';
   @Input() activeColor: string = 'white';
   @Input() borderRadius: string = '9999px';
+  @Input() size: 'small' | 'medium' | 'large' = 'medium';
 
+  height = '28px';
+  width = 'auto';
+  fontSize = '0.875rem'; // text-sm
+  horizontalGap = '12px';
   isHovered: boolean = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['size']) this.updateSize();
+  }
+
+  ngOnInit(): void {
+    this.updateSize();
+  }
+
+  updateSize() {
+    switch (this.size) {
+      case 'large':
+        this.height = '48px';
+        this.fontSize = '1.7rem';
+        this.horizontalGap = '40px';
+        break;
+      case 'small':
+        this.height = '28px';
+        this.fontSize = '1rem';
+        this.horizontalGap = '16px';
+        break;
+      case 'medium':
+      default:
+        this.height = '38px';
+        this.fontSize = '1.4rem';
+        this.horizontalGap = '24px';
+        break;
+    }
+  }
 }
