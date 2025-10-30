d8e5b8e7bf34749b4082b04b3b56c7a7ba045f63
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-10-30 15:16:31 +0530

    Changed the Earnings , reimbursment and Deduction by using notice-banner

--- orms.client/src/app/modules/payroll/salary-structure/salary-structure.component.ts
+++ orms.client/src/app/modules/payroll/salary-structure/salary-structure.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NoticeBannerComponent } from '../../../common-ui/feature-components/notice-banner/notice-banner';
 
 @Component({
   selector: 'app-salary-structure',
   templateUrl: './salary-structure.component.html',
-  styleUrl: './salary-structure.component.css'
+  styleUrl: './salary-structure.component.css',
+  standalone: true,
+  imports: [NoticeBannerComponent]
 })
 export class SalaryStructureComponent {
 
