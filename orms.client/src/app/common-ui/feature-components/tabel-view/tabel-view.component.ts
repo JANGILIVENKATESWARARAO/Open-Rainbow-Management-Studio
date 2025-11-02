b2955b9fbeba3de12855ebc0231cc7edc30a9aa5
Manu Maganti <manu.maganti@gmail.com>
2025-11-02 23:07:31 +0530

    Created  a table-view component

---
+++ orms.client/src/app/common-ui/feature-components/tabel-view/tabel-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabel-view',
+  templateUrl: './tabel-view.component.html',
+  styleUrl: './tabel-view.component.css'
+})
+export class TabelViewComponent {
+
+}
---
+++ orms.client/src/stories/common-ui/feature-components/table-view.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { TabelViewComponent } from '../../../app/common-ui/feature-components/tabel-view/tabel-view.component';
+
+const meta: Meta<TabelViewComponent> = {
+  title: 'Feature Components/TabelViewComponent',
+  component: TabelViewComponent,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  args: {},
+};
+
+export default meta;
+type Tooltip = StoryObj<TabelViewComponent>;
+
+export const dot: TabelViewComponent = {};
