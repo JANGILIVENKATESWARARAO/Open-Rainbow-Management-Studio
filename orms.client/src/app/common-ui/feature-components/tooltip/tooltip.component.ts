4537861221a4d23ddcbeacf1867c060925bae5b7
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-11-04 17:59:13 +0530

    Changes the bgColor, textColor and max-width from static to dynamic  on tooltip

--- orms.client/src/app/common-ui/feature-components/tooltip/tooltip.component.ts
+++ orms.client/src/app/common-ui/feature-components/tooltip/tooltip.component.ts
@@ -1,13 +1,26 @@
-import { Component, ElementRef, Input } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, ElementRef, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tooltip',
   templateUrl: './tooltip.component.html',
   styleUrl: './tooltip.component.css',
   standalone: true,
+  imports : [NgStyle],
 })
 export class TooltipComponent {
   @Input() text = '';
   @Input() position: 'top' | 'bottom' | 'left' | 'right' = 'top';
+  @Input() bgColor: string = '';
+  @Input() textColor: string = '';
+  @Input() maxWidth: string = '';
+
+   @HostBinding('style.--tooltip-bg') get tooltipBg() {
+    return this.bgColor;
+  }
+
+  @HostBinding('style.--tooltip-color') get tooltipColor() {
+    return this.textColor;
+  }
   constructor(public elRef: ElementRef<HTMLElement>) {}
 }
