9b785396979d413fb0bac809a52efbf88b94db31
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-11-06 17:03:56 +0530

    Convert upcoming-holidays component from static to dynamic

--- orms.client/src/app/modules/holidays/upcoming-holidays/upcoming-holidays.component.ts
+++ orms.client/src/app/modules/holidays/upcoming-holidays/upcoming-holidays.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ChipComponent } from '../../../common-ui/controls/chip/chip.component';
+import { CommonModule } from '@angular/common';
+
+export interface HolidayLeave {
+  holidayname: string;
+  holidaydate: string;
+  holidaylist: 'National Holiday' | 'federal Holiday' | 'Festival Holiday';
+}
 
 @Component({
   selector: 'app-upcoming-holidays',
   templateUrl: './upcoming-holidays.component.html',
   styleUrl: './upcoming-holidays.component.css',
   standalone: true,
-  imports:[ChipComponent]
+  imports: [ChipComponent, CommonModule],
 })
 export class UpcomingHolidaysComponent {
+  todayDate: Date = new Date();
+  @Input() holidayleave: HolidayLeave[] = [];
+  
 
+  calculateDays(holidaydate: string, todayDate: Date): number {
+    const from = new Date(holidaydate);
+    const to = new Date(todayDate || holidaydate);
+    return Math.ceil((+from - +to) / 86400000) + 1;
+  }
 }
