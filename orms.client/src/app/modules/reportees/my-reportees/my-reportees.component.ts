d17dfb399345f6fd50e5c6fec924f42a88772cc6
JANGILIVENKATESWARARAO <jangilivenkateswararao@gmail.com>
2025-11-06 16:11:39 +0530

    Added rows in my-reportees and update in dynamic

--- orms.client/src/app/modules/reportees/my-reportees/my-reportees.component.ts
+++ orms.client/src/app/modules/reportees/my-reportees/my-reportees.component.ts
@@ -1,21 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { ButtonComponent } from '../../../common-ui/controls/button/button.component';
+import { CommonModule } from '@angular/common';
 
+interface MyReportees{
+  EmployeePhoto:string;
+  employeeName:string;
+  employeeId:string;
+  employeeEmail:string;
+  designation:string;
+  department:string;
+  workLocation:string;
+
+}
 @Component({
   selector: 'app-my-reportees',
   templateUrl: './my-reportees.component.html',
   styleUrl: './my-reportees.component.css',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent,CommonModule],
 })
 export class MyReporteesComponent {
 
-  @Input() EmployeePhoto: string =
-      'https://plus.unsplash.com/premium_photo-1661757403301-ae68e1f1b827?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735';
-  @Input() employeeName: string = 'Arjuna Sivam Chandhran';
-  @Input() employeeId: string = '9203';
-  @Input() employeeEmail: string = 'arjunasiv@gmail.com';
-  @Input() designation: string = 'Desig1';
-  @Input() department: string = 'Dept1';
-  @Input() workLocation: string = 'Head Office';
+  @Input() myReportees: MyReportees[] = [];
 }
