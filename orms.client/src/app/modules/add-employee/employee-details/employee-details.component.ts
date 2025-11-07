af63bd2436d38635ae73f3f7119c75ab00421ebc
p.polineni <p.polineni@P-POLINENI-WS>
2025-11-07 10:01:50 +0000

    Employee-details component in dynamic way

--- orms.client/src/app/modules/add-employee/employee-details/employee-details.component.ts
+++ orms.client/src/app/modules/add-employee/employee-details/employee-details.component.ts
@@ -3,9 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
-  styleUrl: './employee-details.component.css',
+  styleUrls: ['./employee-details.component.css'],
   standalone: true,
 })
 export class EmployeeDetailsComponent {
-
+  employee = {
+    name: 'Arjuna Sivam Chandhran',
+    designation: 'Desig1',
+    id: 9203,
+    email: 'arjunasivam@gmail.com',
+    gender: 'Male',
+    department: 'Dept1',
+    doj: '14/06/1997',
+    location: 'Head Office',
+  };
 }
--- orms.client/src/stories/modules/add-employee/employee-details.stories.ts
+++ orms.client/src/stories/modules/add-employee/employee-details.stories.ts
@@ -1,15 +1,25 @@
 import type { Meta, StoryObj } from '@storybook/angular';
-import { fn } from 'storybook/test';
 import { EmployeeDetailsComponent } from '../../../app/modules/add-employee/employee-details/employee-details.component';
 
 const meta: Meta<EmployeeDetailsComponent> = {
-  title: 'Modules/AddEmployee/EducationDetails',
+  title: 'Modules/AddEmployee/EmployeeDetails',
   component: EmployeeDetailsComponent,
   tags: ['autodocs'],
   parameters: {
-  layout: 'fullscreen',
+    layout: 'fullscreen',
   },
-  args: {},
+  args: {
+    employee: {
+      name: 'Arjuna Sivam Chandhran',
+      designation: 'Desig1',
+      id: 9203,
+      email: 'arjunasivam@gmail.com',
+      gender: 'Male',
+      department: 'Dept1',
+      doj: '14/06/1997',
+      location: 'Head Office',
+    },
+  },
 };
 
 export default meta;
