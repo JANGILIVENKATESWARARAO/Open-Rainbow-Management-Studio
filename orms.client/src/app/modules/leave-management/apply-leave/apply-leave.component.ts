4c9e07ea4fa5fee7b657b931ae50be941937c1ec
Ganesh-Pradhan <ganeshpradjan1109@gmail.com>
2025-11-10 11:52:51 +0530

    Create a model for apply leave form and store the data of the form in that model

--- orms.client/src/app/modules/leave-management/apply-leave/apply-leave.component.ts
+++ orms.client/src/app/modules/leave-management/apply-leave/apply-leave.component.ts
@@ -4,27 +4,45 @@ import { CalendarComponent } from '../../../common-ui/controls/calendar/calendar
 import { TextAreaComponent } from '../../../common-ui/controls/text-area/text-area.component';
 import { ButtonComponent } from '../../../common-ui/controls/button/button.component';
 import { DropDownComponent } from '../../../common-ui/controls/drop-down/drop-down.component';
+import { CommonModule } from '@angular/common';
+import { ApplyLeave } from '../../../common-ui/assests/view-models/common-view-models';
 
 @Component({
-    selector: 'orms-apply-leave',
-    templateUrl: './apply-leave.component.html',
-    styleUrl: './apply-leave.component.css',
-    imports: [NoticeBannerComponent, CalendarComponent, TextAreaComponent, ButtonComponent, DropDownComponent]
+  selector: 'app-apply-leave',
+  templateUrl: './apply-leave.component.html',
+  styleUrl: './apply-leave.component.css',
+  imports: [
+    NoticeBannerComponent,
+    CalendarComponent,
+    TextAreaComponent,
+    ButtonComponent,
+    DropDownComponent,
+    CommonModule,
+  ],
+  standalone: true,
 })
 export class ApplyLeaveComponent {
-    showForm: boolean = false; 
-   
+  showForm: boolean = false;
 
-  
+  applyLeave: ApplyLeave = new ApplyLeave();
   showLeaveForm() {
-    
-    this.showForm = !this.showForm; 
+    this.showForm = !this.showForm;
   }
 
   CloseApplication() {
     this.showForm = false;
   }
-  
 
+  formatDate(date: Date | null): string {
+    if (!date) return '';
+    const d = new Date(date);
+    const day = String(d.getDate()).padStart(2, '0');
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const year = d.getFullYear();
+    return `${day}-${month}-${year}`;
+  }
 
+  submitApplication() {
+    console.log(this.applyLeave);
+  }
 }
