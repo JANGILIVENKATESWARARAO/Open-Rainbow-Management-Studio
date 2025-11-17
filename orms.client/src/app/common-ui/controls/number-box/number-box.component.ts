572f789007d6d86c9d46303e7ed0d51a1c83de9e
Ganesh-Pradhan <ganeshpradjan1109@gmail.com>
2025-11-17 15:56:53 +0530

    Make changes on border style , make dynamic to the placeholder and add Output decorator in number-box component

--- orms.client/src/app/common-ui/controls/number-box/number-box.component.ts
+++ orms.client/src/app/common-ui/controls/number-box/number-box.component.ts
@@ -1,14 +1,14 @@
-import { Component,Input } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-    selector: 'orms-number-box',
-    templateUrl: './number-box.component.html',
-    styleUrl: './number-box.component.css',
-    imports: []
+  selector: 'orms-number-box',
+  templateUrl: './number-box.component.html',
+  styleUrl: './number-box.component.css',
+  imports: [],
 })
 export class NumberBoxComponent {
- @Input() label: string = 'Casual';
- @Input() isRequired: boolean = true;
+  @Input() label: string = 'Casual';
+  @Input() isRequired: boolean = true;
+  @Input() Placeholder: string = '0.0';
+  @Output() numberBox = new EventEmitter<number>();
 }
-
