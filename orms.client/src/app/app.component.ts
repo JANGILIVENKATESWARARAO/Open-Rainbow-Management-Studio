495e1a04ab735876f562411aaa7a6a1eae734548
Ganesh-Pradhan <ganeshpradjan1109@gmail.com>
2025-11-20 11:05:16 +0530

    adding router outlet in app component

--- orms.client/src/app/app.component.ts
+++ orms.client/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './modules/application-layer/layout/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [],
+  imports: [RouterModule],
 })
 export class AppComponent {
+
   
 }
