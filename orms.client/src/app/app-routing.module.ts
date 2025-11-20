d88f0b46e660d7389a0ee967ef31430bd4a6b5b4
Ganesh-Pradhan <ganeshpradjan1109@gmail.com>
2025-11-20 11:15:56 +0530

    Implemeted default and user routing in App Routing Module

--- orms.client/src/app/app-routing.module.ts
+++ orms.client/src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { emp_routes } from './Routes/employee-routing.module';
+import { LoginPageComponent } from './modules/login-page/login-page/login-page.component';
+import { admin_routes } from './Routes/admin-routing.module';
+import { hr_routes } from './Routes/hr-routing.module';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'employee',
+    children: emp_routes,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'admin',
+    children: admin_routes,
+  },
+  {
+    path: 'hr',
+    children: hr_routes,
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
