7a1812ce3fd4751341b232e088410014fdf4cb50
Ganesh-Pradhan <ganeshpradjan1109@gmail.com>
2025-11-20 11:07:16 +0530

    adding router link to menu bar component

--- orms.client/src/app/common-ui/feature-components/menu-bar/menu-bar.component.ts
+++ orms.client/src/app/common-ui/feature-components/menu-bar/menu-bar.component.ts
@@ -1,39 +1,66 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AppRoutingModule } from '../../../app-routing.module';
+import { RouterLink, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'orms-menu-bar',
-    templateUrl: './menu-bar.component.html',
-    styleUrl: './menu-bar.component.css',
-    imports: [CommonModule]
+  selector: 'orms-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrl: './menu-bar.component.css',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterModule],
 })
 export class MenuBarComponent {
- activeIndex: number = 0;
- isSidebarVisible: boolean = true;
+  constructor(private router: Router) {
+    this.router.navigate([this.menuItems[0].route]);
+  }
 
- menuItems = [
-    { label: 'Dashboard', icon: 'fa fa-home', },
-    { label: 'Payslip Access', icon: 'fa fa-file-text-o' },
-    { label: 'Leave Management', icon: 'fa fa-clock-o', badge: 2 },
-    { label: 'Announcements', icon: 'fa fa-bell-o', badge: 5 },
-    { label: 'Calendar', icon: 'fa fa-calender' },
-    { label: 'FAQ & Help', icon: '' },
+  activeIndex: number = 0;
+  isSidebarVisible: boolean = true;
+
+  menuItems = [
+    {
+      label: 'Dashboard',
+      icon: 'fa fa-home',
+      route: 'doc-info',
+    },
+    {
+      label: 'Payslip Access',
+      icon: 'fa fa-file-text-o',
+      route: 'salary-info',
+    },
+    {
+      label: 'Leave Management',
+      icon: 'fa fa-clock-o',
+      badge: 2,
+      route: 'job-info',
+    },
+    {
+      label: 'Announcements',
+      icon: 'fa fa-bell-o',
+      badge: 5,
+      route: 'contact-info',
+    },
+    {
+      label: 'Calendar',
+      icon: 'fa fa-calender',
+      route: 'other-info',
+    },
+    { label: 'FAQ & Help', icon: '', route: 'education-info' },
   ];
 
-
-
-  
   onClick(index: number) {
     console.log(this.menuItems);
-    
-    this.activeIndex = index;
-  
-}
+    console.log(this.menuItems[0].route);
 
-toggleSidebar() {
-  console.log(   this.isSidebarVisible);
-  
+    this.activeIndex = index;
+  }
+
+  toggleSidebar() {
+    console.log(this.isSidebarVisible);
+
     this.isSidebarVisible = !this.isSidebarVisible;
-      console.log(   this.isSidebarVisible);
+    console.log(this.isSidebarVisible);
   }
 }
