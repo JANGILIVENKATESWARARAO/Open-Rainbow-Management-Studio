900c508fd495e5cdcdcffe24c7ee47b3f2f67345
Ganesh-Pradhan <ganeshpradjan1109@gmail.com>
2025-11-20 11:28:41 +0530

    adding AppRoutingModule in story preview.ts

--- orms.client/.storybook/preview.ts
+++ orms.client/.storybook/preview.ts
@@ -1,17 +1,31 @@
-import type { Preview } from '@storybook/angular'
-import { setCompodocJson } from "@storybook/addon-docs/angular";
-import docJson from "../documentation.json";
+import type { Preview } from '@storybook/angular';
+import { applicationConfig } from '@storybook/angular';
+import { importProvidersFrom } from '@angular/core';
+import { AppRoutingModule, routes } from '../src/app/app-routing.module';
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json';
+import { provideRouter, RouterModule } from '@angular/router';
+
 setCompodocJson(docJson);
+
+export const decorators = [
+  applicationConfig({
+    providers: [importProvidersFrom(AppRoutingModule)],
+  }),
+];
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
 };
 
 export default preview;
+function withComponentInputs(): import('@angular/router').RouterFeatures {
+  throw new Error('Function not implemented.');
+}
